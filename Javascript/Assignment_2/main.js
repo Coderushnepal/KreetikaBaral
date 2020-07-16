42f40ae84078262e7cb0c76d9cbfd4223466980b
Kritika <Kreetikabaral@gmail.com>
2020-07-16 22:30:58 +0545

    Complete Javascript Assignment 2

--- Javascript/Assignment_2/main.js
+++ Javascript/Assignment_2/main.js
@@ -50,12 +50,72 @@ function output(array, letter) {
 
 console.log(output(listOfNames, 's'));
 console.log(output(listOfNames, 'a'));
+console.log('\n\n')
 
-// 3
+// Question 3
+// Convert the array of the result in Question 1, in following format
 
-function nextResult(array) {
-    a = result(array);
-    var sum = a.reduce(function(acc, value) {
-        
-    )}
-}
+var res = function(array) {
+	var getArrayFromQ1 = result(array);
+	var organized = getArrayFromQ1.reduce(function(acc, value, index) {
+		delete value.id;
+		acc[index + 1] = value;
+		return acc;
+	}, {});
+	return organized;
+};
+
+console.log(res(listOfNames));
+console.log('\n\n')
+
+
+//Question 4
+//Write a function that returns the longest sequence of consecutive zeroes in a binary string.
+function longestZero(str) {
+	return str.split('1').sort().pop().length;
+  }
+  
+  console.log(longestZero("01100001011000"));
+  console.log(longestZero("100100100"));
+  console.log(longestZero("11111"));
+  console.log(longestZero(""));
+  console.log('\n\n')
+ 
+  
+//Question 5
+//Create a function that takes a string and replaces the vowels with another character.
+//a = 1, e = 2, i = 3, o = 4, u = 5
+
+function replaceVowel(str) {
+	var strArr = str.split('');
+	for (var i = 0; i < str.length; i++) {
+	  var char = str[i].toLowerCase();
+	  switch(char) {
+		case "a":
+			strArr[i] = 1;
+		  break;
+		case "e":
+			strArr[i] = 2;
+		  break;
+		case "i":
+			strArr[i] = 3;
+		  break;
+		case "o":
+			strArr[i] = 4;
+		  break;
+		case "u":
+			strArr[i] = 5;
+		  break;
+		default:
+			strArr[i];                                     
+	  }                                                    
+	                                                       
+	}
+	return strArr.join('');                                // .join() bina array ma return garchha ["K", "R", 3, "T", 3, "K", 1]            
+  }		                                                   // .join() pachi chai comma ma return garchha K,R,3,T,3,K,1 // .join('') pachi comma hatayera return garchha KR3T3K1
+                                                    
+console.log(replaceVowel("KRITIKA"));
+console.log(replaceVowel("karachi"));
+console.log(replaceVowel("chembur"));
+console.log(replaceVowel("khandbari"));
+
