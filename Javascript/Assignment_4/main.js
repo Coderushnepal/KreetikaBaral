dc39f1f0d9d1a7e4289fa5c7b468eebb8f7b894c
Kritika <Kreetikabaral@gmail.com>
2020-07-19 13:58:52 +0545

    Changes in ball bouncing postion

--- Javascript/Assignment_4/main.js
+++ Javascript/Assignment_4/main.js
@@ -7,8 +7,8 @@ btn.onclick = function(e) {
         var newCircle = document.createElement('div');
         newCircle.classList.add('circle');
         newCircle.style.position = "absolute";
-        newCircle.style.top = Math.random() * height + 'px';
-        newCircle.style.left = Math.random() * width + 'px';
+        newCircle.style.top = Math.random() * (height - 20) + 'px';
+        newCircle.style.left = Math.random() * (width - 20) + 'px';
         box.appendChild(newCircle);
         var direction = 1;
         newCircle.addEventListener("click" , function() {
@@ -16,7 +16,7 @@ btn.onclick = function(e) {
                 // var currentTop = newCircle.style.top;
                 var nextTop = parseInt(newCircle.style.top) + 10 * direction;
                 newCircle.style.top = nextTop + "px";
-                if( nextTop >= height || nextTop<= 0 ) {
+                if( nextTop >= (height - 20) || nextTop<= 0 ) {
                    direction *= -1;
                 }
             }, 1000/60)
