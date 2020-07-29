1c774026a345b86033269c2396d571fae689c527
Kritika <Kreetikabaral@gmail.com>
2020-07-29 16:50:28 +0545

    Creation of dash lines in the div

--- Javascript/Assignment_8/JS/createItems.js
+++ Javascript/Assignment_8/JS/createItems.js
@@ -17,12 +17,29 @@ document.body.appendChild(title);
 title.appendChild(heading);
 title.appendChild(lineText);
 
-//-------Creating blanks to enter letters
+//--------------------------Creating blanks to enter letters----------------------------------------
+//creating main div for dashlines
+var letterDiv = document.createElement('div')               
+
+letterDiv.id = 'letterDiv';
+letterDiv.class = 'letterDiv';
+letterDiv.style.color = 'white'
+letterDiv.style.marginTop = '300px'
+letterDiv.style.marginLeft = '500px'
+document.body.appendChild(letterDiv);
+
+//creating dashlines in main div
 function countLetter() {
-    for (var i=0; i<wordLetters.length; i++) {
-        
+    for( var i=0; i < wordLetters.length; i++ ) {
+        var dashline = document.createElement('div');
+        dashline.classList.add('dash');
+        var newDiv = document.getElementById('letterDiv');
+        newDiv.appendChild(dashline);
     }
 }
+countLetter();
+
+
 
 
 
