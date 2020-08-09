87cfbe075b0c3e45e351f4bfc249613a603ff70e
Kritika <Kreetikabaral@gmail.com>
2020-08-09 19:02:44 +0545

    Class Assignment Spinner, Cards, Modal - Day2

--- React/my-project/src/components/main/beers/BeerGrid.js
+++ React/my-project/src/components/main/beers/BeerGrid.js
@@ -1,13 +1,53 @@
 import React, { Component } from "react";
 
-import Header from "../../commons/header/Header";
+import Beer from "./Beer";
+import { Header, Spinner } from "../../commons";
+import { dummyBeersData } from "../../../constants/dummyData";
 
 class BeerGrid extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      beers: [],
+    };
+  }
+
+  scrollParentRef = null;
+
+  fetchBeers = async () => {
+    setTimeout(() => {
+      this.setState({
+        beers: dummyBeersData,
+        isLoading: false,
+      });
+    }, 5000);
+  };
+
+  componentDidMount() {
+    this.fetchBeers();
+  }
+
   render() {
+    const { isLoading } = this.state;
+
     return (
-        <div>
-            <Header />
-        </div>
+      <div>
+        <Header />
+
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <main>
+            <div className="container" ref={(r) => (this.scrollParentRef = r)}>
+              {this.state.beers.map((beer) => (
+                <Beer key={beer.id} info={beer} />
+              ))}
+            </div>
+          </main>
+        )}
+      </div>
     );
   }
 }
---
+++ React/my-project/src/components/main/beers/BeerModal.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import Beer from "./Beer";
+
+import { Modal } from "../../commons";
+
+class BeerModal extends React.Component {
+  render() {
+    const { beer, handleClose, show } = this.props;
+    const {description } = beer;
+
+    return (
+      <Modal show={show} handleClose={handleClose}>
+        <div>
+          <h2>{description}</h2>
+        </div>
+      </Modal>
+    );
+  }
+}
+
+export default BeerModal;
