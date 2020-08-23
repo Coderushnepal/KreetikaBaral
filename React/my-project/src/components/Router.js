375bac61cc67550f91f95ae2a7b9b491e6465e54
Kritika <Kreetikabaral@gmail.com>
2020-08-23 17:02:30 +0545

    Add Private Routing Redux : End of Project - Day 8 (Complete)

--- React/my-project/src/components/Router.js
+++ React/my-project/src/components/Router.js
@@ -1,21 +1,43 @@
 import React from "react";
-import { BrowserRouter, Switch, Route} from "react-router-dom";
+import {
+  Router as BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import history from "../utils/history";
 import * as routes from "../constants/routes";
 
 import Main from "./main";
-import Favourites from "./favourites"
+import Favourites from "./favourites";
+import { PrivateRoute } from "../components/commons";
 
-// const Favourites = () =><h2>I am Favourites.</h2>;
-const AppRouter = () => (
+const Login = ({ history }) => {
+  const isLoggedIn = () => {
+    return !!localStorage.getItem("access_token");
+  };
+
+  const logIn = () => {
+    localStorage.setItem("access_token", "login bhayo");
+    history.push(routes.HOME);
+  };
+
+  return isLoggedIn() ? (
+    <Redirect to={routes.HOME} />
+  ) : (
+    <h1 onClick={logIn}>Login<br/><span>Click Me</span></h1>
+  );
+};
+
+const Router = () => (
   <BrowserRouter history={history}>
     <Switch>
+      <Route exact path={routes.LOGIN} component={Login} />
       <Route exact path={routes.HOME} component={Main} />
-      <Route exact path={routes.FAVOURITES} component={Favourites} />
-      {/* <MainRouter /> */}
+      <PrivateRoute exact path={routes.FAVOURITES} component={Favourites} />
     </Switch>
   </BrowserRouter>
 );
 
-export default AppRouter;
+export default Router;
