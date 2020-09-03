1b07254d587b6a0c99ed611c9ceec1878aa7e5fe
Kritika <Kreetikabaral@gmail.com>
2020-09-03 15:30:49 +0545

    Change folder name

--- Node JS/Class Discussion/todo-api/index.js
+++ Node JS/Class Discussion/todo-api/index.js
@@ -1,12 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 
 import routes from "./src/routes";
 import logger from "./src/utils/logger";
 import genericErrorHandler from "./src/middlewares/genericErrorHandler";
-
-const appPort = 1234;
 
 const loggingMiddleware = (req, res, next) => {
   const url = req.url;
@@ -27,7 +26,9 @@ app.use(loggingMiddleware);
 app.use(routes);
 app.use(genericErrorHandler); //lastma lyayera rakheko ho -- sabbai error haru aayera accululate hune yei ho
 
+dotenv.config();   //node process ko env ma chai env file ko kura inject garchha
+
 //yo function ko kaam - port open bhaisakepachi chai k sisplay garne bhanera console logma aauchha
-app.listen(appPort, () => {
-  console.log(`Listening on port ${appPort}`);
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Listening on port ${process.env.APP_PORT}`);
 });
