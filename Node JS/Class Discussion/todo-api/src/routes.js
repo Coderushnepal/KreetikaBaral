9c1734509b7532449739ca8eab2cd57243f8c569
Kritika <Kreetikabaral@gmail.com>
2020-09-08 22:08:55 +0545

    Remove all other user endpoints except add user - Day9

--- Node JS/Class Discussion/todo-api/src/routes.js
+++ Node JS/Class Discussion/todo-api/src/routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import * as endpoints from "./constants/endpoints";
 import * as userController from "./controllers/user";
-import { validateUserCreation, validateUserUpdate } from "./schemas/user";
+import { validateUserCreation } from "./schemas/user";
 
 const router = Router();
 
@@ -14,21 +14,11 @@ router.get("/", (req, res, next) => {
   });
 });
 
-router.get(endpoints.GET_USERS, userController.getAllUsers);
-
-router.get(endpoints.GET_USER_BY_ID, userController.getUserById);
-
 //post ko lagi
 router.post(
   endpoints.CREATE_USER,
   validateUserCreation,
   userController.createUser
 );
-
-//delete ko lagi
-router.delete(endpoints.DELETE_USER, userController.deleteUser);
-
-//put ko lagi
-router.put(endpoints.UPDATE_USER, validateUserUpdate,userController.updateUser);
 
 export default router;
