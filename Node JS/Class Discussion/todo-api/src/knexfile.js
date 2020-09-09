d70bd86dfb6fe36875a66edc91a01c7201bdf8b8
Kritika <Kreetikabaral@gmail.com>
2020-09-09 23:31:59 +0545

    Add seeds - Day10

--- Node JS/Class Discussion/todo-api/src/knexfile.js
+++ Node JS/Class Discussion/todo-api/src/knexfile.js
@@ -10,12 +10,15 @@ const dbconfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
     // ssl: process.env.ENABLE_SSL
   },
   migrations: {
     directory: "./migrations",
     table: "migrations",
+  },
+  seeds: {
+    directory: "./seeds",
   },
 };
 
